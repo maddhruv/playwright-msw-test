254189d981a04904895393dbb0933bcbfbf3e58d
Dhruv Jain <dhruvjainpenny@gmail.com>
2023-07-27 21:18:01 +0530

    feat: initial setup

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,11 @@
+import { rest } from "msw";
 import { test } from "./test";
 
-test("has message", async ({ page }) => {
+test("has message", async ({ page, worker }) => {
+  await worker.use(
+    rest.get("/api", (req, res, ctx) => {
+      return res(ctx.json({ message: "From Test Interception" }));
+    })
+  );
   await page.goto("/");
 });
